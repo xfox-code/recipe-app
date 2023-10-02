daacf62c74e71321f76cbc90876d8658637e1f8b
lkrbalci <lkrbalci@gmail.com>
2023-10-02 16:15:03 +0300

    finished for first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import RecipeCard from "./RecipeCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipeName, setRecipeName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [description, setDescription] = useState("");
+  const [recipesData, setRecipesData] = useState([]);
+
+  const clickHandler = async () => {
+    const newRecipe = {
+      recipeName,
+      ingredients,
+      description,
+    };
+    let response = await fetch("http://localhost:3000/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...newRecipe, id: uuidv4() }),
+    });
+  };
+
+  useEffect(() => {
+    const getDataHandler = async () => {
+      await fetch("http://localhost:3000/recipes")
+        .then((res) => res.json())
+        .then((data) => setRecipesData(data));
+    };
+
+    getDataHandler();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>MY RECIPES APP</h1>
+      <section className="new-recipe">
+        <label htmlFor="recipeName">
+          <h3>Recipe Name:</h3>
+          <input
+            type="text"
+            name="recipeName"
+            onChange={(event) => setRecipeName(event.target.value)}
+            placeholder="Recipe Name"
+          />
+        </label>
+        <label htmlFor="Ingredientes">
+          <h3>Ingredients:</h3>
+          <textarea
+            placeholder="Ingredients"
+            name="Ingredientes"
+            onChange={(event) => setIngredients(event.target.value)}
+            id=""
+            cols="30"
+            rows="8"
+          ></textarea>
+        </label>
+        <label htmlFor="Description">
+          <h3>Description:</h3>
+          <textarea
+            name="Description"
+            placeholder="Description"
+            onChange={(event) => setDescription(event.target.value)}
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+        </label>
+        <button onClick={clickHandler}>Save</button>
+      </section>
+      <section>
+        <h1>RECIPES</h1>
+        {recipesData.map((recipe) => (
+          <RecipeCard recipe={recipe} />
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/RecipeCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RecipeCard = ({ recipe }) => {
+  return (
+    <div className="recipe-card" key={recipe.id}>
+      <h2>{recipe.recipeName}</h2>
+      <h3>Ingredients</h3>
+      <p>{recipe.ingredients}</p>
+      <h3>Description</h3>
+      <p>{recipe.description}</p>
+    </div>
+  );
+};
+
+export default RecipeCard;

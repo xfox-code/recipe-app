d290e6ee9750bc8edc02c762ee269a14f67ef4ce
lkrbalci <lkrbalci@gmail.com>
2023-10-02 16:37:42 +0300

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = { plugins: [react()], base: "/" };
-  if (command !== serve) {
+  if (command !== "serve") {
     config.base = "/recipe-app";
   }
 
